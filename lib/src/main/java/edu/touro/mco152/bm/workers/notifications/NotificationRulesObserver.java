0f24a1a632d5550542486b5cf1eb194bc2712662
Akiva Jacobson <jacobsonakiva@gmail.com>
2022-01-09 16:10:47 -0500

    Created a SlackManager class that is capable of sending messages to slack channels. Created an Observer class that checks if a message should be sent when it is notified by an observable, and will then inform SlackManager to do so.

---
+++ lib/src/main/java/edu/touro/mco152/bm/workers/notifications/NotificationRulesObserver.java
@@ -0,0 +1,31 @@
+package edu.touro.mco152.bm.workers.notifications;
+
+import edu.touro.mco152.bm.SlackManager;
+import edu.touro.mco152.bm.observers.Observer;
+import edu.touro.mco152.bm.persist.DiskRun;
+
+public class NotificationRulesObserver implements Observer {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void update(Object o) {
+        if(o instanceof DiskRun){
+            DiskRun dr = (DiskRun) o;
+            if(isRunGreaterThanThreshold(dr)) {
+               sendMessage();
+            }
+        }
+    }
+
+    private void sendMessage() {
+        SlackManager slackManager = new SlackManager("Benchmark");
+        slackManager.postMsg2OurChannel(" The Read Benchmark has a 'max time' that exceeds 3 percent of the benchmarks's average time.");
+    }
+
+    private boolean isRunGreaterThanThreshold(DiskRun dr) {
+        return (dr.getRunMax() - dr.getRunAvg()) > (dr.getRunAvg() * .03); //max = 1700 avg = 1650
+    }
+}
