1744a9e0478a92cb8504ae72e4e32c4139aa438d
Akiva Jacobson <jacobsonakiva@gmail.com>
2022-01-09 16:05:58 -0500

    Fully implemented DatabaseUpdater and Gui class to be Observers and compliant with the observer pattern.

--- lib/src/main/java/edu/touro/mco152/bm/persist/DatabaseUpdater.java
+++ lib/src/main/java/edu/touro/mco152/bm/persist/DatabaseUpdater.java
@@ -1,10 +1,23 @@
 package edu.touro.mco152.bm.persist;
 
 import edu.touro.mco152.bm.observers.Observer;
+import jakarta.persistence.EntityManager;
 
 public class DatabaseUpdater implements Observer {
+
+
     @Override
     public void update() {
 
+    }
+
+    @Override
+    public void update(Object o) {
+        if(o instanceof DiskRun) {
+            EntityManager em = EM.getEntityManager();
+            em.getTransaction().begin();
+            em.persist(o);
+            em.getTransaction().commit();
+        }
     }
 }
